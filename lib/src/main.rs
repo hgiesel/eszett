13e495b4b4be9033ad18520f5048747ec19c4248
Henrik Giesel <hengiesel@gmail.com>
2025-12-08 01:26:06 +0900

    feat: make a singular db access

--- lib/src/main.rs
+++ lib/src/main.rs
@@ -1,14 +1,9 @@
 use sqlx::postgres::PgPoolOptions;
 
-#[tokio::main]
-async fn main() -> Result<(), sqlx::Error> {
-    // Create a connection pool
-    //  for MySQL/MariaDB, use MySqlPoolOptions::new()
-    //  for SQLite, use SqlitePoolOptions::new()
-    //  etc.
+async fn query() -> Result<(), sqlx::Error> {
     let pool = PgPoolOptions::new()
         .max_connections(5)
-        .connect("postgres://postgres:password@localhost/test").await?;
+        .connect("postgres://postgres:postgres@localhost:5432/postgres").await?;
 
     // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL/MariaDB)
     let row: (i64,) = sqlx::query_as("SELECT $1")
@@ -17,5 +12,12 @@ async fn main() -> Result<(), sqlx::Error> {
 
     assert_eq!(row.0, 150);
 
+    println!("{}", row.0);
+
     Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    query().await.unwrap();
 }
