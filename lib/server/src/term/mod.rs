5d29c931a92fbc8a473c705ce817f28b705e317d
Henrik Giesel <hengiesel@gmail.com>
2025-12-09 03:40:05 +0900

    feat: rename eszett dao to dao

--- lib/server/src/term/mod.rs
+++ lib/server/src/term/mod.rs
@@ -1,9 +1,8 @@
 use axum::{Json, Router};
 use axum::http::StatusCode;
 use axum::routing::get;
-use eszett_dao::connection::init_pool;
-use eszett_dao::term::query::filter_terms;
-use crate::{CreateUser, User};
+use dao::connection::init_pool;
+use dao::term::query::filter_terms;
 
 async fn list_terms() -> (StatusCode, Json<Vec<String>>) {
     let pool = init_pool().await;
