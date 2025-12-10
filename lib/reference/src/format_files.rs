ac6153ebc7a5f0c853942fec93295cdad197b7ed
Henrik Giesel <hengiesel@gmail.com>
2025-12-11 05:35:45 +0900

    feat: write first impl of formatter

--- lib/reference/src/format_files.rs
+++ lib/reference/src/format_files.rs
@@ -1,84 +1,90 @@
+use std::io::Write;
 use std::collections::BTreeMap;
-use std::path::{Path, PathBuf};
+use std::fs::File;
+use std::path::Path;
 use icu::locale::locale;
-use crate::lexeme_map::{LexemeMap, EMPTY_LEXEMES};
+use crate::lexeme_map::LexemeMap;
 use anyhow::Result;
 use icu::collator::{Collator, CollatorPreferences};
-use icu::collator::options::CollatorOptions;
-use icu::collator::preferences::{CollationCaseFirst, CollationNumericOrdering, CollationType};
-use icu::locale::preferences::LocalePreferences;
-use icu::locale::subtags::language;
-use base::language::Language;
+use icu::collator::options::{CollatorOptions, Strength};
+use unicode_segmentation::UnicodeSegmentation;
 
 #[derive(Clone, PartialOrd, Ord, PartialEq, Eq)]
-enum LatinCharacterClass {
+pub enum LatinCharacterClass {
     A, B, C, D, E, F, G,
     H, I, J, K, L, M, N,
     O, P, Q, R, S, T, U,
     V, W, X, Y, Z, Hash,
 }
 
-#[derive(Clone, PartialOrd, Ord, PartialEq, Eq)]
-enum LatinCharacterClassSpanish {
-    A, B, C, D, E, F, G,
-    H, I, J, K, L, M, N, Nn,
-    O, P, Q, R, S, T, U,
-    V, W, X, Y, Z, Hash,
+pub fn categorize(all: LexemeMap) -> Result<BTreeMap<LatinCharacterClass, LexemeMap>> {
+    let mut map: BTreeMap<LatinCharacterClass, LexemeMap> = BTreeMap::new();
+
+    let prefs: CollatorPreferences = locale!("en").into();
+    let mut options = CollatorOptions::default();
+    options.strength = Some(Strength::Tertiary);
+    let coll = Collator::try_new(prefs, options)?;
+
+    for (lemma, lexemes) in &all.map {
+        let first = lemma.graphemes(true).next().unwrap();
+
+        if coll.compare(first, "a").is_eq() {
+            let found = map.entry(LatinCharacterClass::A).or_insert(LexemeMap {
+                map: BTreeMap::new(),
+            });
+            found.map.insert(lemma.clone(), lexemes.clone());
+
+        } else if coll.compare(first, "b").is_eq() {
+            let found = map.entry(LatinCharacterClass::B).or_insert(LexemeMap {
+                map: BTreeMap::new(),
+            });
+            found.map.insert(lemma.clone(), lexemes.clone());
+
+        } else if coll.compare(first, "c").is_eq() {
+            let found = map.entry(LatinCharacterClass::C).or_insert(LexemeMap {
+                map: BTreeMap::new(),
+            });
+            found.map.insert(lemma.clone(), lexemes.clone());
+
+        } else if coll.compare(first, "d").is_eq() {
+            let found = map.entry(LatinCharacterClass::D).or_insert(LexemeMap {
+                map: BTreeMap::new(),
+            });
+            found.map.insert(lemma.clone(), lexemes.clone());
+
+        } else if coll.compare(first, "e").is_eq() {
+            let found = map.entry(LatinCharacterClass::E).or_insert(LexemeMap {
+                map: BTreeMap::new(),
+            });
+            found.map.insert(lemma.clone(), lexemes.clone());
+
+        } else {
+            let found = map.entry(LatinCharacterClass::Hash).or_insert(LexemeMap {
+                map: BTreeMap::new(),
+            });
+            found.map.insert(lemma.clone(), lexemes.clone());
+        }
+    }
+
+    Ok(map)
 }
-
-struct LocaleKey {
-    pub sort_key: Vec<u8>, // stable ordering
-    pub original: String,
-}
-
-pub fn categorize<LocaleKey>() {
-    // let ag = Collator::try_new(
-    //
-    // );
-    let collator = &locale!("sv");//, CollatorOptions::new()).unwrap();
-
-    let mut map: BTreeMap<LocaleKey, i32> = BTreeMap::new();
-    // println!("{}", map.len());
-
-
-    for word in ["zebra", "åland", "örebro", "apple"] {
-
-        // map.insert(key, 1);
-    };
-
-    // for k in map.keys() {
-    //     println!("{}", k.original);
-    // }
-}
-
 
 pub fn format_files(path: &Path, map: LexemeMap) -> Result<()> {
-    // let prefs = CollatorPreferences {
-    //     locale_preferences: LocalePreferences::default(),
-    //     collation_type: Some(CollationType::Eor),
-    //     case_first: Some(CollationCaseFirst::Lower),
-    //     numeric_ordering: Some(CollationNumericOrdering::False),
-    // };
-    // prefs.locale_preferences = LocalePreferences::default();
-    // prefs.locale_preferences
-    //
-    // let options = CollatorOptions::default().
-    //
-    //
-    // let x = Collator::try_new(prefs, &locale!("en"))?;
-    // let collator = Collator::new(&locale!("fr"), CollatorOptions::new())
-    //     .expect("collator");
-    // //, CollatorOptions::newk()).unwrap();
-    //
-    // println!("{:#?}", collator);
+    let categories = categorize(map)?;
 
-    // for key in map.map.keys() {
-    //     key
-    //
-    //
-    // }
+    for (key, map) in categories {
+        let output_path = path.join(format!("data/en/{}.yaml", match key {
+            LatinCharacterClass::A => "a",
+            LatinCharacterClass::B => "b",
+            LatinCharacterClass::C => "c",
+            LatinCharacterClass::D => "d",
+            LatinCharacterClass::E => "e",
+            _ => "#",
+        }));
 
+        let mut file = File::create(output_path)?;
+        write!(file, "{}", map)?;
+    }
 
-    categorize::<Language>();
     Ok(())
 }
--- lib/reference/src/write_lexemes.rs
+++ lib/reference/src/write_lexemes.rs
@@ -1,8 +1,6 @@
 use std::path::Path;
 use base::language::Language;
 use anyhow::Result;
-use crate::lexeme_map::{LexemeMap, EMPTY_LEXEMES};
-use crate::read_files::read_files;
 use crate::read_lexemes::ReadLexemes;
 use crate::format_files::format_files;
 
